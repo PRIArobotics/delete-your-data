8e47f59d87b23cfaa370f8922c348802c853726e
Clemens Koza <koza@pria.at>
2020-07-21 14:46:23 +0200

    remove unused routes variable

--- server/routes/index.js
+++ server/routes/index.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = app => {
-  const routes = {};
-
   consola.info({
     message: 'Finding all Routes and Controller:',
     badge: true,
@@ -15,7 +13,6 @@ module.exports = app => {
       const route = require(path.join(__dirname, file));
       route.doRouting(app);
       consola.info('Routes found: ' + route.name);
-      routes[route.name] = route;
     });
   consola.log('- - - - - - - - -');
   consola.success('server/routes/index.js');
