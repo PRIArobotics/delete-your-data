47fa15a9de0146c54a605867bbd691b48b2f4b3d
Clemens Koza <koza@pria.at>
2020-07-22 13:08:45 +0200

    add comment to expressify

--- server/apiRoutes/plugin.routes.js
+++ server/apiRoutes/plugin.routes.js
@@ -2,6 +2,11 @@ const { Router } = require('express');
 
 const { Plugin } = require('../controller');
 
+// Converts an async function into an express-conformant request handler
+// If the function is successful (Promise resolves),
+// the function's return value is sent as an http response.
+// If the function is unsuccessful (exception thrown/Promise rejected),
+// that error is passed to express' `next`, triggering any error handling middleware.
 function expressify(asyncHandler) {
   return (req, res, next) => {
     asyncHandler(req)
