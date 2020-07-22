93e5dfbdd64032b4bf262bffe3021cd5f09e5627
Clemens Koza <koza@pria.at>
2020-07-22 12:08:29 +0200

    better error handling via async/await & throw, httperrors, and using next

--- server/controller/plugin.controller.js
+++ server/controller/plugin.controller.js
@@ -1,58 +1,53 @@
+const httpErrors = require('httperrors');
+
 const db = require('../../models');
 
 const Op = db.Sequelize.Op;
 
 module.exports.name = 'Plugin';
 
-module.exports.create = (req, res) => {
-  // get POST data
-  const { name, type, config } = req.body;
+module.exports.create = (req, res, next) => {
+  async function doCreate({ name, type, config }) {
+    // validate data
+    if (!name) {
+      throw new httpErrors[400]('`name` can not be empty!');
+    }
 
-  // validate data
-  if (!name) {
-    res.status(400).send({
-      message: '`name` can not be empty!',
-    });
-    return;
+    if (!type) {
+      throw new httpErrors[400]('`type` can not be empty!');
+    }
+
+    // save to database
+    try {
+      const plugin = await db.Plugin.create({ name, type, config });
+      return plugin;
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
   }
 
-  if (!type) {
-    res.status(400).send({
-      message: '`type` can not be empty!',
-    });
-    return;
-  }
-
-  // save to database
-  const plugin = { name, type, config };
-  db.Plugin.create(plugin)
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || 'An error occurred...',
-      });
-    });
+  doCreate(req.body)
+    .then(data => res.send(data))
+    .catch(next);
 };
 
-module.exports.readAll = (req, res) => {
-  // get GET data
-  const { name } = req.query;
+module.exports.readAll = (req, res, next) => {
+  async function doReadAll({ name }) {
+    // create filter
+    var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
 
-  // create filter
-  var condition = name ? { name: { [Op.like]: `%${name}%` } } : null;
+    // query database
+    try {
+      const plugins = await db.Plugin.findAll({ where: condition });
+      return plugins;
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  }
 
-  // query database
-  db.Plugin.findAll({ where: condition })
-    .then(data => {
-      res.send(data);
-    })
-    .catch(err => {
-      res.status(500).send({
-        message: err.message || 'An error occurred...',
-      });
-    });
+  doReadAll(req.query)
+    .then(data => res.send(data))
+    .catch(next);
 };
 
 module.exports.read = (req, res) => {};
