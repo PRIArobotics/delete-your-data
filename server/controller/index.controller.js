d7d28e709eb644d5db29261ac7e858b82e024657
pmedved-tgm <philipp.medved@gmx.at>
2020-07-23 11:10:40 +0200

    CRUD begin

--- server/controller/index.controller.js
+++ server/controller/index.controller.js
@@ -1,17 +1,78 @@
+const httpErrors = require('httperrors');
+
 const db = require('../../models');
 
 const Op = db.Sequelize.Op;
 
 module.exports.name = 'Index';
 
-module.exports.create = (req, res) => {};
+module.exports.create = async({ user_id, savelocation }) => {
+    // validate data
+  if (!user_id) {
+    throw new httpErrors[400]('`user_id` can not be empty!');
+  }
+
+  if (!savelocation) {
+    throw new httpErrors[400]('`savelocation` can not be empty!');
+  }
+
+  // save to database
+  try {
+    const index = await db.Index.create({ user_id, savelocation });
+    return index;
+  } catch (err) {
+    throw new httpErrors[500](err.message || 'An error occurred...');
+  }
+}
 
 module.exports.readAll = (req, res) => {};
 
 module.exports.read = (req, res) => {};
 
-module.exports.update = (req, res) => {};
+module.exports.update = async (id, { user_id, savelocation}) => {
+    // validate data
+    if (!user_id) {
+      throw new httpErrors[400]('`user_id` can not be empty!');
+    }
+  
+    if (!savelocation) {
+      throw new httpErrors[400]('`savelocation` can not be empty!');
+    }
+  
+    // save to database
+    let num;
+    try {
+      // update returns one or two numbers (usually one, except for special circumstances:
+      // https://sequelize.org/v5/class/lib/model.js~Model.html#static-method-update)
+      // we want the first of those numbers, i.e. do an array destructuring assignment here:
+      [num] = await db.Index.update({ user_id, savelocation }, {
+        where: { id },
+      });
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  
+    if (num !== 1) {
+      throw new httpErrors[400](`Updating Index with ID=${id} failed`);
+    }
+  
+    return { message: 'Index was updated successfully.' };
+  };
 
-module.exports.delete = (req, res) => {};
-
-module.exports.deleteAll = (req, res) => {};
+  module.exports.delete = async (user_id) => {
+    // save to database
+    let num;
+    try {
+      num = await db.Index.destroy({
+        where: { user_id },
+      });
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  
+    if (num !== 1) {
+      throw new httpErrors[400](`Deleting Index with UserID=${user_id} failed`);
+    }
+  
+    return { message: 'Index was deleted successfully.' };
+  };
--- server/controller/user.controller.js
+++ server/controller/user.controller.js
@@ -1,17 +1,79 @@
+const httpErrors = require('httperrors');
+
 const db = require('../../models');
 
 const Op = db.Sequelize.Op;
 
 module.exports.name = 'User';
 
-module.exports.create = (req, res) => {};
+module.exports.create = async({ plugin_uuid, native_id }) => {
+    // validate data
+    if (!plugin_uuid) {
+      throw new httpErrors[400]('`plugin_uuid` can not be empty!');
+    }
+  
+    if (!native_id) {
+      throw new httpErrors[400]('`native_id` can not be empty!');
+    }
+  
+    // save to database
+    try {
+      const user = await db.User.create({ plugin_uuid, native_id});
+      return user;
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  }
 
 module.exports.readAll = (req, res) => {};
 
 module.exports.read = (req, res) => {};
 
-module.exports.update = (req, res) => {};
+module.exports.update = async (id, { plugin_id, native_id}) => {
+    // validate data
+    if (!plugin_id) {
+      throw new httpErrors[400]('`plugin_id` can not be empty!');
+    }
+  
+    if (!native_id) {
+      throw new httpErrors[400]('`native_id` can not be empty!');
+    }
+  
+    // save to database
+    let num;
+    try {
+      // update returns one or two numbers (usually one, except for special circumstances:
+      // https://sequelize.org/v5/class/lib/model.js~Model.html#static-method-update)
+      // we want the first of those numbers, i.e. do an array destructuring assignment here:
+      [num] = await db.User.update({ plugin_id, native_id }, {
+        where: { id },
+      });
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  
+    if (num !== 1) {
+      throw new httpErrors[400](`Updating User with ID=${id} failed`);
+    }
+  
+    return { message: 'User was updated successfully.' };
+  };
 
-module.exports.delete = (req, res) => {};
-
-module.exports.deleteAll = (req, res) => {};
+  module.exports.delete = async (uuid) => {
+    // save to database
+    let num;
+    try {
+      num = await db.User.destroy({
+        where: { uuid },
+      });
+    } catch (err) {
+      throw new httpErrors[500](err.message || 'An error occurred...');
+    }
+  
+    if (num !== 1) {
+      throw new httpErrors[400](`Deleting User with UUID=${uuid} failed`);
+    }
+  
+    return { message: 'User was deleted successfully.' };
+  };
+  
