6758e283f450fc1729a4dd01f9bcc01b523b0741
Clemens Koza <koza@pria.at>
2020-07-27 14:50:37 +0200

    change test plugin name, search with query to be able to shield queries from other concurrent test suites

--- test/controller/plugin.spec.js
+++ test/controller/plugin.spec.js
@@ -12,8 +12,8 @@ describe('Plugin Controller', () => {
     // create
     {
       const plugin = await Plugin.create({
-        name: 'dummy',
-        type: 'dummy',
+        name: 'plugin_test',
+        type: 'plugin_test',
         config: { foo: 0 },
       });
       // toMatchObject because sequelize model instances are not plain objects
@@ -21,8 +21,8 @@ describe('Plugin Controller', () => {
         uuid: expect.any(String),
         createdAt: expect.any(Date),
         updatedAt: expect.any(Date),
-        name: 'dummy',
-        type: 'dummy',
+        name: 'plugin_test',
+        type: 'plugin_test',
         config: { foo: 0 },
       });
 
@@ -31,15 +31,15 @@ describe('Plugin Controller', () => {
 
     // read all
     {
-      const plugins = await Plugin.readAll({});
+      const plugins = await Plugin.readAll({ name: 'plugin_test' });
       // toMatchObject because sequelize model instances are not plain objects
       expect(plugins).toMatchObject([
         {
           uuid: expect.any(String),
           createdAt: expect.any(Date),
           updatedAt: expect.any(Date),
-          name: 'dummy',
-          type: 'dummy',
+          name: 'plugin_test',
+          type: 'plugin_test',
           config: { foo: 0 },
         },
       ]);
@@ -48,8 +48,8 @@ describe('Plugin Controller', () => {
     // update
     {
       await Plugin.update(uuid, {
-        name: 'dummy',
-        type: 'dummy',
+        name: 'plugin_test',
+        type: 'plugin_test',
         config: { bar: 0 },
       });
     }
@@ -62,8 +62,8 @@ describe('Plugin Controller', () => {
         uuid: expect.any(String),
         createdAt: expect.any(Date),
         updatedAt: expect.any(Date),
-        name: 'dummy',
-        type: 'dummy',
+        name: 'plugin_test',
+        type: 'plugin_test',
         config: { bar: 0 },
       });
     }
@@ -72,7 +72,7 @@ describe('Plugin Controller', () => {
     {
       await Plugin.del(uuid);
 
-      expect(await Plugin.readAll({})).toHaveLength(0);
+      expect(await Plugin.readAll({ name: 'plugin_test' })).toHaveLength(0);
     }
   });
 });
