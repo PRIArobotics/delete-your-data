47e1709a6412768b8af4c48471dca403c80954c8
Clemens Koza <koza@pria.at>
2020-08-07 10:28:44 +0200

    add setup of the DYD service to dummy test. Now, the test suite is roughly ready for testing the interaction between DYD and a service.

--- test/dummy.spec.js
+++ test/dummy.spec.js
@@ -2,18 +2,43 @@ import http from 'http';
 import util from 'util';
 import request from 'supertest';
 
-// import appPromise from '@/server/app';
+import appPromise from '@/server/app';
 import dummyAppPromise from '@/dummyService/app';
 
-let dummyServer;
+let dummyServer, appServer, pluginUuid;
 
 beforeAll(async () => {
+  // start dummy service
   dummyServer = http.createServer(await dummyAppPromise);
   await util.promisify(dummyServer.listen).call(dummyServer, 0);
   const dummyApiUrl = `http://localhost:${dummyServer.address().port}/api`;
+
+  // start DYD service
+  appServer = http.createServer(await appPromise);
+  await util.promisify(appServer.listen).call(appServer, 0);
+
+  // register dummy plugin
+  const res = await request(appServer)
+    .post('/api/plugin')
+    .send({
+      name: 'dummy_test_plugin',
+      type: 'Dummy',
+      config: { apiUrl: dummyApiUrl },
+    });
+  expect(res.statusCode).toEqual(200);
+  pluginUuid = res.body.uuid;
 });
 
 afterAll(async () => {
+  // unregister dummy plugin
+  const res = await request(appServer)
+    .delete(`/api/plugin/${pluginUuid}`)
+    .send();
+
+  // stop DYD service
+  await util.promisify(appServer.close).call(appServer);
+
+  // stop dummy service
   await util.promisify(dummyServer.close).call(dummyServer);
 });
 
