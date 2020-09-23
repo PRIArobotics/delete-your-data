b4d2e2bff4afc6f4176865115c02c48d466e1b7e
Clemens Koza <koza@pria.at>
2020-09-23 14:55:08 +0200

    separate client and server implementation of the dummy plugin; the client variant can validate, but can't execute redaction commands (obviously)

---
+++ dyd-dummy-plugin/src/index.client.js
@@ -0,0 +1,51 @@
+import { isWebUri } from 'valid-url';
+
+/**
+ * The dummy plugin connects to DYD dummy service, which in turn manages a number of accounts
+ * identified by a username, and a number of entries for each account with text content.
+ *
+ * The service makes its capabilities available via a Rest API.
+ */
+export default class DummyPlugin {
+  static TYPE_NAME = 'Dummy';
+
+  // All methods here must not access external resources; they do data validation only.
+  // This should allow them to be used client-side.
+
+  /**
+   * Check that the passed value is a valid DummyPlugin config.
+   */
+  static validateConfig(config) {
+    const { apiUrl, ...rest } = config;
+
+    const restEntries = Object.entries(rest);
+    if (restEntries.length !== 0) {
+      const msg = restEntries.map(([key, value]) => `${key}=${value}`).join(', ');
+      throw new Error(`config has unexpected entries: ${msg}`);
+    }
+
+    if (isWebUri(apiUrl)) {
+      throw new Error(`apiUrl must be a legal http or https URL`);
+    }
+  }
+
+  constructor({ apiUrl }) {
+    this.apiUrl = apiUrl;
+  }
+
+  /**
+   * Check that the passed value is a valid DummyPlugin account ID.
+   * This plugin expects the ID to be a username, i.e. a string.
+   */
+  validateAccountId(id) {
+    if (typeof id !== 'string') throw new Error(`account id must be a string`);
+  }
+
+  /**
+   * Check that the passed value is a valid DummyPlugin location description.
+   * This plugin expects the location to be an integer.
+   */
+  validateLocation(location) {
+    if (typeof location !== 'number') throw new Error(`account id must be a number`);
+  }
+}
--- dyd-dummy-plugin/src/index.server.js
+++ dyd-dummy-plugin/src/index.server.js
@@ -1,5 +1,9 @@
-import { isWebUri } from 'valid-url';
 import axios from 'axios';
+
+// this is a Node-only API
+import fs from 'fs';
+
+import BrowserDummyPlugin from './index.client';
 
 /**
  * The dummy plugin connects to DYD dummy service, which in turn manages a number of accounts
@@ -7,49 +11,7 @@ import axios from 'axios';
  *
  * The service makes its capabilities available via a Rest API.
  */
-export default class DummyPlugin {
-  static TYPE_NAME = 'Dummy';
-
-  /**
-   * Check that the passed value is a valid DummyPlugin config.
-   */
-  static validateConfig(config) {
-    const { apiUrl, ...rest } = config;
-
-    const restEntries = Object.entries(rest);
-    if (restEntries.length !== 0) {
-      const msg = restEntries.map(([key, value]) => `${key}=${value}`).join(', ');
-      throw new Error(`config has unexpected entries: ${msg}`);
-    }
-
-    if (isWebUri(apiUrl)) {
-      throw new Error(`apiUrl must be a legal http or https URL`);
-    }
-  }
-
-  constructor({ apiUrl }) {
-    this.apiUrl = apiUrl;
-  }
-
-  /**
-   * Check that the passed value is a valid DummyPlugin account ID.
-   * This plugin expects the ID to be a username, i.e. a string.
-   */
-  validateAccountId(id) {
-    if (typeof id !== 'string') throw new Error(`account id must be a string`);
-  }
-
-  /**
-   * Check that the passed value is a valid DummyPlugin location description.
-   * This plugin expects the location to be an integer.
-   */
-  validateLocation(location) {
-    if (typeof location !== 'number') throw new Error(`account id must be a number`);
-  }
-
-  // All methods above must not access external resources; they do data validation only.
-  // This should allow them to be used client-side.
-
+export default class DummyPlugin extends BrowserDummyPlugin {
   // The methods below can access databases etc.
   // The `mode` parameter must be either `DELETE` or `ANONYMIZE`, and specifies whether records
   // in the referenced data store are to be deleted or only cleared of personal information.
